794552ad30620ef4b426580657fa7f278c3f2256
yinawake <yinawake@gmail.com>
2013-10-27 08:49:52 +0800

    add html5 python

---
+++ code/python_/watch_word.py
@@ -0,0 +1,7 @@
+'''
+找出匹配的字符串 by yinchao
+'''
+#re.split("\s+","sss xxx fff    vvvv xxxx   ttt") 分割字符串  以空格
+#s = set([12,23,45,67])    d = set([33,22,45,67])
+#s | d     s & d      s - d    s  ^  d
+ 
